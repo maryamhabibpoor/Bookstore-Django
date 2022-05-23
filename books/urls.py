13b34b6715554dda079892a326a324e8c3dbd932
unknown <maryamhabibpoor0812@tutanota.com>
2022-05-23 18:48:50 +0430

    add BookCreateView

--- books/urls.py
+++ books/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import BookListView
 from .views import BookDetailView
-
+from .views import BookCreateView
 
 urlpatterns = [
     path('', BookListView.as_view(), name='book_list'),
     path('<int:pk>/',BookDetailView.as_view(), name ='book_detail'),
+    path('create/', BookCreateView.as_view(), name='book_create'),
 ]
