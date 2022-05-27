a3af2a615b0fb23531e35703a7e1ebdff21079b5
unknown <maryamhabibpoor0812@tutanota.com>
2022-05-27 14:40:23 +0430

    edit delete and update button

--- books/views.py
+++ books/views.py
@@ -22,7 +22,7 @@ class BookCreateView(generic.CreateView):
 
 class BookUpdateView(generic.UpdateView):
     model = Book
-    fields = ['title', 'author', 'description', 'price']
+    fields = ['title', 'author', 'description', 'price', 'cover',]
     template_name = 'books/book_update.html'
 
 
