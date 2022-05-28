e43cfe090b75ce7d2b48402af838196945e5c8cb
unknown <maryamhabibpoor0812@tutanota.com>
2022-05-28 09:46:59 +0430

    add comment to the model

--- books/admin.py
+++ books/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Comment
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display=('user','book','text','datetime_created',)
+
 
 admin.site.register(Book)
+admin.site.register(Comment,CommentAdmin)
 
-# Register your models here.
