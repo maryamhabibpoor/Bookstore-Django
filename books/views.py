0af635e4315c42acdf715763c6351b3141ee291a
unknown <maryamhabibpoor0812@tutanota.com>
2022-05-29 12:46:01 +0430

    add permission function to the views

--- books/views.py
+++ books/views.py
@@ -3,7 +3,7 @@ from .models import Book
 from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404, render
 from .forms import CommentForm
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.decorators import login_required
 
 
@@ -47,13 +47,21 @@ class BookCreateView(LoginRequiredMixin, generic.CreateView):
     template_name = 'books/book_create.html'
 
 
-class BookUpdateView(LoginRequiredMixin, generic.UpdateView):
+class BookUpdateView(LoginRequiredMixin,UserPassesTestMixin, generic.UpdateView):
     model = Book
     fields = ['title', 'author', 'description', 'price', 'cover', ]
     template_name = 'books/book_update.html'
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.user == self.request.user
 
-class BookDeleteView(LoginRequiredMixin, generic.DetailView):
+
+class BookDeleteView(LoginRequiredMixin,UserPassesTestMixin, generic.DetailView):
     model = Book
     template_name = 'books/book_delete.html'
     success_url = reverse_lazy('book_list')
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.user == self.request.user
